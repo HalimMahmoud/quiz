fc2080d751626fe6e6a6e8ad5fd1c60970a26083
momensale7 <momensaleh2468@gmail.com>
2025-05-10 18:59:18 +0300

    fix conflict

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { Toaster } from "sonner";
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AuthLayout from "./modules/SharedModules/AuthLayout/AuthLayout";
-import Login from "./modules/AdminModules/Authentication/Login/Login";
 import Register from "./modules/AdminModules/Authentication/Register/Register";
 import ChangePassword from "./modules/AdminModules/Authentication/ChangePassword/ChangePassword";
 import VerifyAccount from "./modules/AdminModules/Authentication/VerifyAccount/VerifyAccount";
 import ResetPassword from "./modules/AdminModules/Authentication/ResetPass/ResetPassword";
 import ForgetPassword from "./modules/AdminModules/Authentication/ForgetPassword/ForgetPassword";
 import NotFound from "./modules/SharedModules/NotFound/NotFound";
+import Login from "./modules/AdminModules/Authentication/Login/Login";
 
 function App() {
   const router = createBrowserRouter([
